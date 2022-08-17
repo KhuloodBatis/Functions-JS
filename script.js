5bac2bcbd666343136b1f5da45ff7f5e87111fca
KhuloodBatis <k.batis@bimventures.com>
2022-08-17 10:03:14 +0300

    kool

--- script.js
+++ script.js
@@ -251,34 +251,143 @@
 // Hints: Use many of the tools you learned about in this and the last section 😉
 // GOOD LUCK 😀
 
-const poll = {
-question: "What is your favourite programming language?",
-options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
-// This generates [0, 0, 0, 0]. More in the next section!
-answers: new Array(4).fill(0),
-registerNewAnswer(){
-    //?GEt answer
-    const answer = Number( prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
-    console.log(answer);
-    //?REGISTER answer
-    typeof answer ==='number' && answer < this.answers.length && this.answers[answer]++;
+// const poll = {
+// question: "What is your favourite programming language?",
+// options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+// // This generates [0, 0, 0, 0]. More in the next section!
+// answers: new Array(4).fill(0),
+// registerNewAnswer(){
+//     //?GEt answer
+//     const answer = Number( prompt(`${this.question}\n${this.options.join('\n')}\n(Write option number)`));
+//     console.log(answer);
+//     //?REGISTER answer
+//     typeof answer ==='number' && answer < this.answers.length && this.answers[answer]++;
 
-    // console.log(this.answers);
-    this.displayResults();
-    this.displayResults('string');
-},
-displayResults(type = 'array'){
-if (type === 'array'){
-    console.log(this.answers);
-}else if ( type === 'string'){
-    //poll result are 13,2,4,1 
-    console.log(`Poll result are ${this.answers.join(', ')}`);
-}
-}
-};
-// poll.registerNewAnswer();
+//     // console.log(this.answers);
+//     this.displayResults();
+//     this.displayResults('string');
+// },
+// displayResults(type = 'array'){
+// if (type === 'array'){
+//     console.log(this.answers);
+// }else if ( type === 'string'){
+//     //poll result are 13,2,4,1 
+//     console.log(`Poll result are ${this.answers.join(', ')}`);
+// }
+// }
+// };
+// // poll.registerNewAnswer();
 
-document.querySelector('.poll')
-.addEventListener('click', poll.registerNewAnswer.bind(poll))
+// document.querySelector('.poll')
+// .addEventListener('click', poll.registerNewAnswer.bind(poll))
 
-poll.displayResults.call({answers:[5,2,3]},'string')
+// poll.displayResults.call({answers:[5,2,3]},'string')
+
+
+//?136 
+
+// const runOnce = function (){
+//     console.log('This will never run again');
+//     const isPrivate = 23;
+// }
+// runOnce();
+
+// console.log(isPrivate);
+
+//?function expressions 
+
+// (function (){
+//     console.log('This will never run again');
+// })();//= calling this function 
+
+// (() => console.log('This will ALSO never run again'))();
+
+// {
+//     const isPrivate = 23;
+//     var notPrivate = 46;  
+// }
+
+// // console.log(isPrivate);
+// console.log(notPrivate);
+
+//?CLOSURES
+
+// const secureBooking = function(){
+//     let passengerCount = 0 ;
+//     return function(){
+//         passengerCount++;
+//         console.log(`${passengerCount} passengers`);
+//     }
+// }
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+// console.dir(booker)
+// secureBooking();//did not work 
+
+//?example1 for closures 
+// let f ;
+
+// const g = function(){
+//     const a =23 ;
+//     f = function(){
+//         console.log(a*2);
+//     }
+// }
+
+// const h = function(){
+//     const b = 777;
+//     f = function(){
+//         console.log(b*2);
+//     }
+// }
+
+// g();
+// f();
+// console.dir(f);
+
+// // Re_assigning f function
+// h();
+// f();
+// console.dir(f);
+// //? Example 2 
+
+// const boardPassengers = function(n,wait){
+//     const perGroup = n /3 ;
+
+//      setTimeout(function(){
+//         console.log(`We are now boarding al ${n} passengers `);
+//         console.log(`There are 3 groups, each with${perGroup} passengers`);
+//      },wait*1000);
+
+
+//      console.log(`Will start boarding in ${wait}  second`);
+// }
+
+// boardPassengers(180 , 3)
+
+//!+++++====Chalengs
+
+// The Complete JavaScript Course 22
+// Coding Challenge #2
+// This is more of a thinking challenge than a coding challenge 🤓
+// Your tasks:
+// 1. Take the IIFE below and at the end of the function, attach an event listener that
+// changes the color of the selected h1 element ('header') to blue, each time
+// the body element is clicked. Do not select the h1 element again!
+// 2. And now explain to yourself (or someone around you) why this worked! Take all
+// the time you need. Think about when exactly the callback function is executed,
+// and what that means for the variables involved in this example.
+
+(function () {
+const header = document.querySelector('h1');
+header.style.color = 'red';
+
+document.querySelector('body').addEventListener('click',function(){
+    header.style.color = 'blue'; 
+})
+})();
+// GOOD LUCK 
